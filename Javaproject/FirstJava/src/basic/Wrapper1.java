9894901bf1a2c5b7752391a2ebeaa6d7ef3ee291
NamSeoA <tjdk97@gmail.com>
2020-10-22 17:47:04 +0900

    classCode

--- Javaproject/FirstJava/src/basic/Wrapper1.java
+++ Javaproject/FirstJava/src/basic/Wrapper1.java
@@ -7,9 +7,9 @@ public class Wrapper1 {
 		Integer i1 = new Integer(100);   // 인스턴스 생성
 		Integer i2 = new Integer("100"); // 인스턴스 생성
 		
-		System.out.println("참조변수의 비교 => i1==i2 : " + (i1==i2)); //false
-		System.out.println("저장하는 value값의 비교 => i1.equals(i2) : "+ i1.equals(i2)); //true
-		System.out.println("i1.toString() : " + i1.toString()); //100
+		System.out.println("참조변수의 비교 => i1==i2 : " + (i1==i2)); //false (주소값 다름)
+		System.out.println("저장하는 value값의 비교 => i1.equals(i2) : "+ i1.equals(i2)); //true (equals는 내용 비교를 함)
+		System.out.println("i1.toString() : " + i1.toString()); //100 (i를 문자열로 바꿔서 출력)
 		System.out.println("i2.toString() : " + i2.toString());
 		
 		System.out.println("Integer.MAX_VALUE : "+ Integer.MAX_VALUE);
